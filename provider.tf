17c7b97acbc7857b6116001cc919880a472a9219
firas <firas.ouerghi@etudiant-enit.utm.tn>
2021-03-19 14:52:27 +0100

    Add providers [google, google-beta]

---
+++ provider.tf
@@ -0,0 +1,20 @@
+
+
+
+provider "google" {
+
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+
+
+provider "google-beta" {
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
